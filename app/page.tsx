19868bdd00e99fa88603184581d4c9d6042a7bf9
Puskar-Roy <puskarroy600@gmail.com>
2024-04-29 12:07:25 +0530

    feat : gallery coursol

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { ModeToggle } from "@/components/Theme-Button";
+import { CarouselDemo } from "@/components/Coursol";
+import { Alata } from "next/font/google";
+
+const alata = Alata({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function Home() {
   return (
-    <main className="">
-      Hello
-      <ModeToggle />
+    <main className="mt-[100px]">
+      <div className="w-[85%] mx-auto">
+        <div className="flex flex-col justify-center items-center gap-8">
+          <h1 className={`text-4xl font-semibold ${alata.className}`}>
+            My <span className={`text-rose-500`}>Memories</span>
+          </h1>
+          <div className=" mx-auto flex justify-center items-center">
+            <CarouselDemo />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/Coursol.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import logo from '@/public/profilepic.jpeg'
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs sm:max-w-[70rem]">
+      <CarouselContent>
+        <CarouselItem className="basic-1/3 sm:basis-1/5">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={logo} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/5">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={logo} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/5">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={logo} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/5">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={logo} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/5">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={logo} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
