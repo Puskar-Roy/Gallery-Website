9735a806e08a55c3fbd979ba92f04a246d77480e
Puskar-Roy <puskarroy600@gmail.com>
2024-05-01 21:12:16 +0530

    fix : next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,13 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handeler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-};
-
-const handeler = NextAuth(authOptions);
+});
 
 export { handeler as GET, handeler as POST };
