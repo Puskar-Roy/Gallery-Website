d469c1ed820263ca726944ec2d20057dc72d2f2b
Puskar-Roy <puskarroy600@gmail.com>
2024-05-06 00:06:39 +0530

    feat : images

--- components/Coursol.tsx
+++ components/Coursol.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import logo from '@/public/profilepic.jpeg'
+import logo from '@/public/firstpic.jpg'
+import pic1 from '@/public/mypic.jpg'
+import pic2 from '@/public/newpic.jpg'
+import pic3 from '@/public/oldpic.jpg'
+import pic4 from '@/public/oldpic2.jpg'
+import pic5 from '@/public/oldpic3.jpg'
+import pic6 from '@/public/ppic.jpg'
+import pic8 from '@/public/hero1.jpg'
+import pic9 from '@/public/hero2.jpg'
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -18,7 +26,25 @@ export function CarouselDemo() {
           <div className="p-1">
             <Card>
               <CardContent className="flex aspect-square items-center justify-center p-6">
-                <Image src={logo} height={600} width={600} alt="Logo" />
+                <Image src={pic3} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/4">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={pic4} height={600} width={600} alt="Logo" />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basic-1/3 sm:basis-1/4">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <Image src={pic3} height={600} width={600} alt="Logo" />
               </CardContent>
             </Card>
           </div>
@@ -36,7 +62,7 @@ export function CarouselDemo() {
           <div className="p-1">
             <Card>
               <CardContent className="flex aspect-square items-center justify-center p-6">
-                <Image src={logo} height={600} width={600} alt="Logo" />
+                <Image src={pic2} height={600} width={600} alt="Logo" />
               </CardContent>
             </Card>
           </div>
@@ -45,7 +71,7 @@ export function CarouselDemo() {
           <div className="p-1">
             <Card>
               <CardContent className="flex aspect-square items-center justify-center p-6">
-                <Image src={logo} height={600} width={600} alt="Logo" />
+                <Image src={pic5} height={600} width={600} alt="Logo" />
               </CardContent>
             </Card>
           </div>
@@ -54,7 +80,7 @@ export function CarouselDemo() {
           <div className="p-1">
             <Card>
               <CardContent className="flex aspect-square items-center justify-center p-6">
-                <Image src={logo} height={600} width={600} alt="Logo" />
+                <Image src={pic1} height={600} width={600} alt="Logo" />
               </CardContent>
             </Card>
           </div>
